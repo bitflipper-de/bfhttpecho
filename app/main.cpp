34a080298b782178efa6b75e2cf0aaabb635e9b3
Robert Jakob <rj@robertjakob.de>
2019-12-08 16:31:38 +0100

    Initial project structure.

---
+++ app/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "bfhttpecho.h"
+
+int main(int argc, char* argv[])
+{
+    std::cout << "bfhttpecho " << BFHTTPECHO_VERSION << std::endl;
+    return 0;
+}
---
+++ config/default.cmake
@@ -0,0 +1,1 @@
+set(BFHTTPECHO_BUILD_TESTS ON CACHE BOOL "Enables building the tests of bfhttpecho")
